86a3e98891996d164a948f141427354eb015f04a
Udvalkhorol <59464028+Udvalkhorol@users.noreply.github.com>
2021-11-09 22:13:53 +0800

    Add files via upload
    Halloween xD

---
+++ p4.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Controller from './components/controller/Controller';
+
+ReactDOM.render(
+  <Controller />,
+  document.getElementById('p4'),
+);
---
+++ p5.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Controllersub from './components/controller/Controllersub';
+
+ReactDOM.render(
+  <Controllersub />,
+  document.getElementById('p5'),
+);
